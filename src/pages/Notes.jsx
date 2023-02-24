a51ad4c33c54c8a002d08fd390edc9d07bcd25a8
Sofia Genchi <sofigen16@gmail.com>
2023-02-24 13:52:13 -0300

    notes page design

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Notes = () => {
   return (
-    <div>Notes</div>
+    <section>
+      <header className='notes__header'>
+        <h2>My Notes</h2>
+        <input type="text" autoFocus placeholder='Keyword...' />
+        <button></button>
+      </header>
+    </section>
   )
 }
 
